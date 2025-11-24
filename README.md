c3d4e4f1220ea66bdf9ef0bad55d6823af48ff40
Aldo Yair Arias Torrero Becerra <arias.aldo333@gmail.com>
2025-11-24 16:48:07 -0600

    Mi primer commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Pruebas para el parámetro firstName al crear un/a usuario/a en API_GROCERS_STORE
+⦁	Necesitas tener instalados los paquetes pytest y request para ejecutar las pruebas.
+⦁	Ejecuta todas las pruebas con el comando pytest.
+Aldo Yair te desea un bonito día desde México
---
+++ sender_stand_request.py
@@ -0,0 +1,15 @@
+import configuration
+import requests
+import data
+
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+response = post_new_user(data.user_body);
+print(response.status_code)
+print(response.json())
